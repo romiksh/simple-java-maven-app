64f88eef785cd5942a85083e1685f986b4cddd34
Shyov Roman <shilov.ra@gmail.com>
2020-09-18 15:04:11 -0400

    Update build.sh

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -4,5 +4,5 @@ echo "***********************"
 echo "** Building Java App **"
 echo "***********************"
 
-docker run --rm -v $PWD:/app -w /app maven:3-alpine "$@"
+docker run --rm -v /var/jenkins_home/workspace/testpipe:/app -w /app maven:3-alpine "$@"
 #cp ./target/*.jar ./docker/
